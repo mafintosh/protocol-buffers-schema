c1b7c26f065402d77810be1df11317526f191648
Mathias Buus <mathiasbuus@gmail.com>
2014-07-28 21:36:50 +0200

    added tokenizer

---
+++ tokenize.js
@@ -0,0 +1,36 @@
+module.exports = function(sch) {
+  var noComments = function(line) {
+    var i = line.indexOf('//')
+    return i > -1 ? line.slice(0, i) : line
+  }
+
+  var noMultilineComments = function() {
+    var inside = false
+    return function(token) {
+      if (token === '/*') {
+        inside = true
+        return false
+      }
+      if (token === '*/') {
+        inside = false
+        return false
+      }
+      return !inside
+    }
+  }
+
+  var trim = function(line) {
+    return line.trim()
+  }
+
+  return sch
+    .replace(/([;,{}=\[\]])/g, ' $1 ')
+    .split(/\n/)
+    .map(trim)
+    .filter(trim)
+    .map(noComments)
+    .filter(trim)
+    .join('\n')
+    .split(/\s+|\n+/gm)
+    .filter(noMultilineComments())
+}
