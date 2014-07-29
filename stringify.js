6e461640785b0c016d227bd7690c54eb899abba7
Mathias Buus <mathiasbuus@gmail.com>
2014-07-29 10:02:29 +0200

    stringify is more defensive

--- stringify.js
+++ stringify.js
@@ -1,7 +1,7 @@
 var onfield = function(f, result) {
   var prefix = f.repeated ? 'repeated' : f.required ? 'required' : 'optional'
 
-  var opts = Object.keys(f.options).map(function(key) {
+  var opts = Object.keys(f.options || {}).map(function(key) {
     return key+' = '+f.options[key]
   }).join(',')
 
@@ -14,14 +14,17 @@ var onfield = function(f, result) {
 var onmessage = function(m, result) {
   result.push('message '+m.name+' {')
 
+  if (!m.enums) m.enums = []
   m.enums.forEach(function(e) {
     result.push(onenum(e, []))
   })
 
+  if (!m.messages) m.messages = []
   m.messages.forEach(function(m) {
     result.push(onmessage(m, []))
   })
 
+  if (!m.fields) m.fields = []
   m.fields.forEach(function(f) {
     result.push(onfield(f, []))
   })
@@ -53,10 +56,12 @@ module.exports = function(schema) {
   var result = []
   if (schema.package) result.push('package '+schema.package+';', '')
 
+  if (!schema.enums) schema.enums = []
   schema.enums.forEach(function(e) {
     onenum(e, result)
   })
 
+  if (!schema.messages) schema.messages = []
   schema.messages.forEach(function(m) {
     onmessage(m, result)
   })
