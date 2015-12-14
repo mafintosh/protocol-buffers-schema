3e956e82566fbc752e0c05793a13d305bca24f52
Conor <conor_barber@apple.com>
2015-12-13 20:09:14 -0600

    roll enum value options into the value field and convert into an object. Also add support for enum level options

--- parse.js
+++ parse.js
@@ -240,35 +240,35 @@ var onsyntaxversion = function (tokens) {
 }
 
 var onenumvalue = function (tokens) {
-  var options = {}
   if (tokens.length < 4) throw new Error('Invalid enum value: ' + tokens.slice(0, 3).join(' '))
   if (tokens[1] !== '=') throw new Error('Expected = but found ' + tokens[1])
   if (tokens[3] !== ';' && tokens[3] !== '[') throw new Error('Expected ; or [ but found ' + tokens[1])
 
   var name = tokens.shift()
   tokens.shift()
-
-  var value = Number(tokens.shift())
-
+  var val = {
+    value: null,
+    options: {}
+  }
+  val.value = Number(tokens.shift())
   if (tokens[0] === '[') {
-    options = onfieldoptions(tokens)
+    val.options = onfieldoptions(tokens)
   }
   tokens.shift() // expecting the semicolon here
 
   return {
     name: name,
-    value: value,
-    options: options
+    val: val
   }
 }
 
 var onenum = function (tokens) {
   tokens.shift()
-
+  var options = {}
   var e = {
     name: tokens.shift(),
     values: {},
-    valueoptions: {}
+    options: {}
   }
 
   if (tokens[0] !== '{') throw new Error('Expected { but found ' + tokens[0])
@@ -282,15 +282,12 @@ var onenum = function (tokens) {
       return e
     }
     if (tokens[0] === 'option') {
-      // just skip "option allow_alias = true;"
-      while (tokens.shift() !== ';') {
-        // do nothing
-      }
+      options = onoption(tokens)
+      e.options[options.name] = options.value
+      continue
     }
     var val = onenumvalue(tokens)
-    // console.log(val)
-    e.values[val.name] = val.value
-    e.valueoptions[val.name] = val.options
+    e.values[val.name] = val.val
   }
 
   throw new Error('No closing tag for enum')
