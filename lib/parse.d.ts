4a9ac3061c8174303d7835be1b7edf819bd09087
Zane Hannan <zanehannanau@gmail.com>
2019-04-18 14:40:25 +1000

    remove lib

--- lib/parse.d.ts
+++
@@ -1,7 +0,0 @@
-import { Schema } from "./schema";
-interface ToString {
-    toString(): string;
-}
-export declare function parse<T extends ToString>(from: T): Schema;
-export {};
-//# sourceMappingURL=parse.d.ts.map
--- lib/tokenise.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const tokenise: (s: string) => string[];
-export default tokenise;
-//# sourceMappingURL=tokenise.d.ts.map
