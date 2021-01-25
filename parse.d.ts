1bc58169defb08c6ec67824cf3bc1ed21d826bc2
achingbrain <alex@achingbrain.net>
2021-01-25 18:45:02 +0000

    feat: add types
    Takes the types from [@types/protocol-buffers-schema](https://www.npmjs.com/package/@types/protocol-buffers-schema)
    and adds them to this module.
    
    It swaps node `Buffer`s for `Uint8Array`s in the interfaces because
    this module doesn't use any `Buffer` features that aren't available
    on `Uint8Array`s which makes it a bit kinder to the browser.

---
+++ parse.d.ts
@@ -0,0 +1,9 @@
+import { Schema } from "./types";
+declare namespace parse {
+    function parse(buffer: string | Uint8Array): Schema;
+    function stringify(schema: Schema): string;
+}
+
+declare function parse(buffer: string | Uint8Array): Schema;
+
+export = parse;
